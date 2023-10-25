321d79c5b16fc893686c609c3e2c8fc617408a41
tranthanhhthao <90049000+tranthanhhthao@users.noreply.github.com>
2023-10-25 22:41:08 +0700

    Add files via upload

---
+++ essays.js
@@ -0,0 +1,50 @@
+const essays = [{
+        title: 'low cost airlines',
+        prompt: 'The increasing availability of low-cost airlines now lets people travel around the world. Some feel this is a positive development while others think it is negative overall.',
+        question: 'Discuss both sides and give your own opinion.',
+        link: '/low-cost-airlines',
+        essay: ['It is great to get to travel around the world---and low-cost airlines have made this easier. One learns a lot from travel. But should one learn anyhow from discomfort?', 'The greatest advantage of low-cost airline is the least money spent on means of transport. When we focus on the fact that we save money for the trip, we save money for what actually matters; on the plane, we look forward to spending more money on the fun and pleasure after landing. Such as, buying more things, eating more good food; because these things cannot be bought on the plane.', 'But before we get to the destination, does every penny count? Cheap seats of lower comforts and the lack of some services are what low-cost airline offers. Our time on the plane counts as much as time travel at destination. Because it is part of the trip. It can be a really bad start filled with terrible smells. It can ruin everything. Aren\'t time well-spent and valuable experience what make the entire trip worthwhile?', 'To conclude, I see that the disadvantages in cheap seats are many and too much to take. I count every second on the means to get there. They say flighting first-class always be some luxury to experience.']
+    }, 
+    {
+        title: 'sport and learning',
+        prompt: 'Some people encourage watching sports as a way of learning about teamwork and strategy, while others believe that one can learn these skills through playing sports.',
+        question: 'Discuss both sides and give your own opinion.',
+        link: '/sport-and-learning'
+    }, 
+    {
+        title: 'Ads target kids',
+        prompt: 'In some countries, there has been an increase in the number of advertisements which try to persuade children to buy snacks, toys, and other goods. Parents often claim that these ads are unfair.',
+        question: 'Discuss both sides and give your own opinion.',
+        link: '/ads-target-kids'
+    }, 
+    {
+        title: 'imported supermarkets',
+        prompt: 'Many supermarkets are selling more and more products that are imported from other countries instead of selling food that is locally sourced.',
+        question: 'What are the reasons for this? Is this a positive or negative development?',
+        link: '/imported-supermarkets'
+    }, 
+    {
+        title: 'cycling license',
+        prompt: 'Some experts say for road safety cyclists should pass a test before being allowed on public roads.',
+        question: 'To what extent do you agree or disagree?',
+        link: '/cycling-license'
+    }, 
+    {
+        title: 'old age and youth',
+        prompt: 'In some places, old age is valued, while in other cultures youth is considered more important.',
+        question: 'Discuss both views and give your opinion.',
+        link: '/old-age-and-youth', 
+        essay: ['While elders are paid the utmost respect in places, youth is of the greatest things elsewhere.', 'My culture teaches me to value old age the most. Confucianism emphasizes filial piety. Seeing an old person is like seeing somebody\’s mom or dad. They are of great value because of their lifelong experience and wisdoms. It is culturally unacceptable to wrong old people at any matter. As a culture, we hold in high regard the quality of being traditional and conservative. ', 'But, I feel a bit suffocating at home every now and then. And I find myself attracted to youth cultures elsewhere. ', 'Youth in America seems excitingly different. They are great even when they rebel without a cause. Their decisions are not underestimated because they lack real life experience. They can decide everything for themselves. They of course can make bad choices—and that is okay—as long as they have the adventure and the self-discovery. It seems that young people are encouraged to be brand new, to break old patterns and think outside the box. Which probably because the American culture admires creativity and resistance much. ', 'Personally, I\’m not a fan of Confucianism in my culture. I do respect old people but to an extent. I don\’t like conservatives, who are mostly old. ']
+    }, {
+        title: 'reading for children',
+        prompt: 'Some people believe that engaging in an active pastime does more to develop childrens\’ life skills than time spent reading.',
+        question: 'To what extent do you agree or disagree?',
+        link: '/reading-for-children',
+        essay: ['Many people would rather children spending time doing other things than reading. They consider reading not an active pastime and not very helpful developing life skills at a young age. I disagree—I think reading is essential to growth.', 'First, reading requires and strengthens children\’s ability to concentrate and self-discipline—it is not an passive thing to do. While reading, they need to constantly reflect on what they have known and read. After reading, they often arrive at an intellectual discussion with the writers while coming to senses with their understanding. In short, reading is a form of communicating attentively. ', 'Secondly, reading exercises the brain and helps children improve language skills. It allows them to comprehend the meaning of language, literacy, and social skills. It also gets them ready for academic life. In other words, the benefits of reading are many and are for children as well. Therefore, it is right to encourage children to read in their free time. ', 'To conclude, it\’s actually great to allow children to read while letting them set up their own schedules. Remember to pick a reading list that suits their reading-level so they can feel engaging and have a chance to challenge themselves to level up. ']
+    }
+    
+    
+
+];
+
+export default essays;
---
+++ main.js
@@ -0,0 +1,64 @@
+import essays from './essays.js'
+
+const essayBoxes = document.querySelectorAll('.essay');
+
+const shuffleBttn = document.getElementById('shuffle');
+
+console.log(essays.length)
+
+function shuffle () {
+    let order = [];
+
+    function pushUniqueRandom(array) {
+        const randomNum = Math.floor(Math.random() * essays.length)
+
+        function isUnique(num, array) {
+            for (let i = 0; i < array.length; i++) {
+                if (num === array[i]) {
+                    return false
+                }
+            } 
+            return true
+        }
+
+        if (isUnique(randomNum, array)) {
+            array.push(randomNum)
+        } 
+    }
+
+    function getOrder(array) {
+        pushUniqueRandom(array)
+        if (array.length === 6) {
+            return 'error'
+        } else if (array.length < 6) {
+            getOrder(array)
+            return array
+        }
+    }
+
+    order = getOrder(order)
+    console.log(order)
+
+
+    function addEssay (box, index) {
+
+        box.innerHTML = ''
+
+        box.href = '#';
+        box.appendChild(document.createElement('h3')).innerHTML = essays[index].title;
+        box.appendChild(document.createElement('p')).innerHTML = essays[index].prompt;
+        box.appendChild(document.createElement('p')).innerHTML = essays[index].question;
+
+    }
+
+    // essayBoxes.forEach(box => addEssay(box));
+    for (let i = 0; i < order.length; i++) {
+        addEssay(essayBoxes[i], order[i])
+    }
+}
+
+document.addEventListener('load', shuffle())
+shuffleBttn.addEventListener('click', () => {
+    shuffle();
+    console.log('clicked')
+})
